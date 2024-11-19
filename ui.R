1ebc5b8564ded17f9ec6f165c308f723ad301259
cipheLab <37363841+cipheLab@users.noreply.github.com>
2024-11-19 13:34:45 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -4,40 +4,65 @@ library(shinydashboard)
 library(shinyjs)
 library(shinybusy)
 library(flowCore)
-library(shiny)
 library(shinythemes)
 
-ui <- fluidPage(
-  theme = shinytheme("flatly"),  # Applying a theme
+ui <- dashboardPage(
+  dashboardHeader(title = "FCS Anonymiser"),
   
-  titlePanel("FCS Fluorochrome Anonymiser", windowTitle = "FCS Anonymiser"),
-  
-  tags$head(
-    tags$style(HTML("
-      .info-text {
-        color: red; 
-        font-style: italic;
-        margin-bottom: 20px;
-      }
-    "))
-  ),
-  
-  fluidRow(
-    column(12,
-           tags$h4("Please make sure that all the uploaded FCS files have the same column names!", 
-                   class = "info-text")
-    )
-  ),
-  
-  sidebarLayout(
-    sidebarPanel(
-      fileInput('file1', 'Choose FCS File', accept = c('.fcs'), multiple = TRUE),
-      actionButton('downloadData', 'Download Modified File'),
-
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Help", tabName = "help", icon = icon("question")),
+      menuItem("Upload", tabName = "upload", icon = icon("upload"))
     ),
-    mainPanel(
-      DTOutput('table')  ,# Display the interactive table
-      actionButton("changeNames", "Anonymise"),
+    
+  actionButton('downloadData', 'Download Modified File', 
+                   style = "margin-top: 20px;")
+  ),
+  
+  dashboardBody(
+    tags$head(
+      tags$style(HTML("
+        .info-text {
+          color: red; 
+          font-style: italic;
+          margin-bottom: 20px;
+        }
+        .custom-header {
+          text-align: center;
+          margin-bottom: 20px;
+        }
+      "))
+    ),
+    
+    tabItems(
+      tabItem(tabName = "upload",
+              fluidRow(
+                column(12,
+                       tags$h3("Please ensure that all uploaded FCS files have the same column names!", 
+                               class = "info-text"),
+                       tags$hr(),  # Horizontal line for better separation
+                       fileInput('file1', 'Choose FCS File', 
+                                 accept = c('.fcs'), multiple = TRUE),
+                       tags$br(),  # Line break for spacing
+                       actionButton("changeNames", "Anonymise", 
+                                    class = "btn btn-primary")  # Button with styling
+                ),
+                column(12, DTOutput('table'),  # Display the interactive table
+                       tags$br(),  # Line break for spacing
+                       tags$div(id = "status_message")  # Placeholder for status messages
+                )
+              )
+      ),
+      tabItem(tabName = "help",
+              h2("This tool is a R Shiny application that anonymizes fluorochromes in one or more FCS files."),
+              h3("Instructions:"),
+              tags$ol(
+                tags$li("Load FCS file(s)"),
+                tags$li("Select lines to be anonymized"),
+                tags$li("Click on the ‘Anonymise’ button"),
+                tags$li("Download the modified FCS file")
+              )
+      )
     )
   )
 )
